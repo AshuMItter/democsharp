b923ea0cd6e6184a35696bbaa725623ffa79073e
AshuMItter <84823182+AshuMItter@users.noreply.github.com>
2022-03-08 16:42:31 +0530

    Program.cs Updted One

--- Program.cs
+++ Program.cs
@@ -3,50 +3,6 @@ using System.Text;
 using DemoOne.Concepts;
 namespace DemoOne
 {
-   class MyProperties
-    {
-        int _age = 45;
-        string _name=" ";
-
-        public string Name {
-
-            get {
-               
-                return _name;
-            }
-            set
-            {
-
-                _name = value;
-            }
-        
-        
-        }
-        public int Age {
-
-            get
-            {
-
-                return _age;
-            }
-
-            set
-            {
-                if (value <= 0)
-                {
-                    Console.WriteLine("Wrong Entry! Value did not save");
-                }
-                else
-                {
-                    _age = value;
-                }
-            }
-        
-        }
-
-
-
-    }
     class Program
     {
 
