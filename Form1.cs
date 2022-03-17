02f115f8d47407ea8e2f707cc26270bcf9843db4
AshuMItter <84823182+AshuMItter@users.noreply.github.com>
2022-03-17 13:36:22 +0530

    Form1.cs contains GridView

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Console.WriteLine("Hello World!");
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=TempDB; Integrated Security=true");
+            con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM ForSQL1 inner JOIN ForSQL2 ON ForSQL1.ProductID = ForSQL2.ProductID", con);
+            DataSet setDs = new DataSet();
+
+            sda.Fill(setDs);
+            dataGridView1.DataSource = setDs.Tables[0];
+
+            con.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=TempDB; Integrated Security=true");
+            con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM ForSQL1 inner JOIN ForSQL2 ON ForSQL1.ProductID = ForSQL2.ProductID", con);
+            DataSet setDs = new DataSet();
+
+            sda.Fill(setDs);
+            dataGridView1.DataSource = setDs.Tables[0];
+
+            con.Close();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,122 +1,23 @@
-﻿using System;
-using System.Data;
-using System.Data.SqlClient;
+using System;
 using System.Collections.Generic;
-namespace ConsoleAppAdoDotNet
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
-            ReadDataFromDbaseConnected();
-        }
-        static void ReadDataFromDbaseConnected()
-        {
-          SqlConnection con = new SqlConnection("Data Source =.; Initial Catalog = TempDB;Integrated Security = true");
-          con.Open();
-          SqlCommand com = new SqlCommand("Select min(age) from ForSQL1", con);
-
-SqlDataReader rdr = com.ExecuteReader();
-
-            while (rdr.Read())
-            {
-                Console.WriteLine(rdr.GetInt32(0));
-            }
-        //  Console.WriteLine(val);
-            //  //   SqlDataReader sdr = com2.ExecuteReader();
-
-            //     SqlDataReader read = com.ExecuteReader();
-            //     //com.ExecuteNonQuery();
-            //      List<ForSQL1> items = new List<ForSQL1>();
-
-            //    // Console.WriteLine(obj.);
-            //     ForSQL1 sql1 = null;
-            //     while (read.Read())
-            //     {
-            //       sql1 = new ForSQL1();
-            //       sql1.ProductID =    read.GetInt32(0);
-            //       sql1.Age =   read.GetInt32(1);
-            //       sql1.NameP =   read.GetString(2);
-            //       sql1.LocationP =   read.GetString(3);
-
-            //         items.Add(sql1);
-
-            //         //items.Add(sql1);
-            //         //Console.WriteLine(read.GetInt32(0));
-            //         //Console.WriteLine(read.GetInt32(1));
-            //         //Console.WriteLine(read.GetString(2));
-            //         //Console.WriteLine(read.GetString(3));
-
-            //     }
-            //     con.Close();
-            //  //   con.Dispose();
-            //     foreach (var item in items)
-            //     {
-            //         Console.WriteLine($"Hi {item.NameP} ! I found your ID was :{item.ProductID} while you are from {item.LocationP}. You seem to be grown up as your age is {item.Age}");
-            //     }
-            con.Close();
-            con.Dispose();
-           
-            //SqlCommand com2 = new SqlCommand("Select * from ForSQL1", con);
-
-            //SqlDataReader sdr1 = com2.ExecuteReader();
-
-            //while (sdr1.Read())
-            //{
-            //    Console.WriteLine(sdr1.GetInt32(0));
-            //    Console.WriteLine(sdr1.GetInt32(1));
-            //    Console.WriteLine(sdr1.GetString(2));
-            //    Console.WriteLine(sdr1.GetString(3));
-
-
-            //}
-            //con.Close();
-            //con.Dispose();
-
-
-
-            //foreach (var item in items)
-            //{
-            //    Console.WriteLine(item.NameP);
-            //}
-        }
-        static void ReadDataFromDbaseDisConnected()
-        {
-            SqlConnection con = new SqlConnection("Data Source =.; Initial Catalog = TempDB;Integrated Security = true");
-            con.Open();
-          //  SqlCommand com = new SqlCommand("Select * from ForSQL1", con);
-
-            SqlDataAdapter sda = new SqlDataAdapter("Select * from ForSQL1", con);
-
-            DataSet ds = new DataSet();
-            sda.Fill(ds, "ForSQL1");
-
-            DataTable dt = ds.Tables[0];
-
-            DataRow dr = dt.Rows[3];
-            foreach (var item in dr.ItemArray)
-            {
-                Console.WriteLine(item);
-            }
-
-            DataColumn dc = dt.Columns[0];
-
-            Console.WriteLine(dc.Caption);
-            //Console.WriteLine(dt.);
-
-            con.Close();
-            con.Dispose();
-
-        }
-        class ForSQL1
-        {
-            public int ProductID { get; set; }
-            public int Age { get; set; }
-
-            public string NameP { get; set; }
-
-            public string LocationP { get; set; }
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
